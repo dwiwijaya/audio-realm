e09c114c66dbece2284a1dc3b73944314143dcb1
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-05-17 16:30:46 +0700

    style: pageheading margin top

--- src/components/common/PageHeading.jsx
+++ src/components/common/PageHeading.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
   const PageHeading = ({ title, description }) => {
     return (
-      <div className="mt-6">
-        <h2 className="relative font-bold text-xl text-text dots">{title}</h2>
+      <div className="mt-4">
+        <h2 className="relative font-bold text-lg text-text dots flex items-center gap-2"><i className="bx bx-book-alt h-fit"></i> {title} </h2>
         <p className="leading-5 mb-4 text-subtext text-sm">
           {description}
         </p>
