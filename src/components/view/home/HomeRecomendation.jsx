c24376c0eff969403c3884562b037fd401c3bef0
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-18 23:51:09 +0700

    add: item recomendation

--- src/components/view/home/HomeRecomendation.jsx
+++ src/components/view/home/HomeRecomendation.jsx
@@ -7,17 +7,29 @@ const HomeRecomendation = () => {
         <div className=''>
             <PageHeading icon="bx bx-like" title="Curated Recommendations" description="Discover Top Picks and Must-Have Gear Selected by Our Audiophile Experts" />
             <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-                <Link href="/" className="card flex items-center justify-center">
-                    IDR 0-500K
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 0-300K IDR
                 </Link>
-                <Link href="/" className="card flex items-center justify-center">
-                    IDR 500K-1000K
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 300K-500K IDR
                 </Link>
-                <Link href="/" className="card flex items-center justify-center">
-                    IDR 1000K-2000K
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 500K-1000K IDR
                 </Link>
-                <Link href="/" className="card flex items-center justify-center">
-                    IDR 2000K++
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 1000K-2000K IDR
+                </Link>
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 2000K-3000K IDR
+                </Link>
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 3000k-4000K IDR
+                </Link>
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-wallet"></div> 4000K-5000K IDR
+                </Link>
+                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
+                    <div className="bx bx-infinite"></div> UNLIMITED
                 </Link>
             </div>
         </div>
