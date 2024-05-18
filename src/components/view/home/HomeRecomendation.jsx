88ff0fbf1ab517e499ef488263e47be1c460a001
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-19 00:09:08 +0700

    style: hover effect recomendation card

--- src/components/view/home/HomeRecomendation.jsx
+++ src/components/view/home/HomeRecomendation.jsx
@@ -5,31 +5,31 @@ import React from 'react'
 const HomeRecomendation = () => {
     return (
         <div className=''>
-            <PageHeading icon="bx bx-like" title="Curated Recommendations" description="Discover Top Picks and Must-Have Gear Selected by Our Audiophile Experts" />
+            <PageHeading icon="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-like" title="Curated Recommendations" description="Discover Top Picks and Must-Have Gear Selected by Our Audiophile Experts" />
             <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 0-300K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet rote"></div> 0-300K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 300K-500K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 300K-500K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 500K-1000K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 500K-1000K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 1000K-2000K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 1000K-2000K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 2000K-3000K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 2000K-3000K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 3000k-4000K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 3000k-4000K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-wallet"></div> 4000K-5000K IDR
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 4000K-5000K IDR
                 </Link>
-                <Link href="/" className="text-sm card flex gap-2 items-center justify-center">
-                    <div className="bx bx-infinite"></div> UNLIMITED
+                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-infinite"></div> UNLIMITED
                 </Link>
             </div>
         </div>
