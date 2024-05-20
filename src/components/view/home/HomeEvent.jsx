33c503fd318b795190e89be6398eac6d0b4a2d4c
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-20 21:52:03 +0700

    style: event homepage

--- src/components/view/home/HomeEvent.jsx
+++ src/components/view/home/HomeEvent.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 
 const HomeEvent = () => {
   return (
-    <div className='card !rounded-lg'>
+    <div className='card !rounded-lg relative'>
+
       <h1 className="text-lg flex items-center gap-2 font-bold">
-        <i className="bx bx-calendar"></i> Upcoming Audiophile Events <span className="badge bg-secondary">4</span>
+        <i className="bx bx-calendar"></i> Upcoming Community Events
       </h1>
-      <p className='text-sm mt-1'>Stay Tuned for Exciting Gatherings, Workshops, and Exhibitions in the Audiophile Community</p>
-      <Link className='group btn btn-sm bg-secondary flex items-center mt-3 mb-1' href='/'>
-        Browse Events <i className="bx bx-right-arrow-alt group-hover:ml-1 transition-base"></i>
-      </Link>
+      <p className='text-sm mt-1'> Join us in our upcoming events and connect with fellow audiophiles. Stay tuned for exciting gatherings and discussions!</p>
+      <div className="flex items-center mt-5 mb-1 gap-1">
+        <span className="!px-3 btn btn-sm bg-secondary">4</span>
+        <Link className='group btn btn-sm bg-secondary ' href='/'>
+          Browse Events <i className="bx bx-right-arrow-alt group-hover:ml-1 transition-base"></i>
+        </Link>
+      </div>
     </div>
   )
 }
