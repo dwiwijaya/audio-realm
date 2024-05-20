7620c569bc6d62b20b1bb59c65b8c0b1f08b42ea
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-05-20 14:21:14 +0700

    add: icon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         />
+        <link rel="icon"  href="/favicon/icon.png" sizes="any"/>
       </Head>
       <body>
         <Main />
