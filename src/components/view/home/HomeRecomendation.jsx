530049183a3e4d4aa7c409015e1a1adb1f210f95
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-20 21:06:38 +0700

    add: term for recomendation class

--- src/components/view/home/HomeRecomendation.jsx
+++ src/components/view/home/HomeRecomendation.jsx
@@ -1,4 +1,5 @@
 import PageHeading from '@/components/common/PageHeading'
+import { RECOMENDATION_CLASS } from '@/constant/recomendation'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,30 +8,11 @@ const HomeRecomendation = () => {
         <div className=''>
             <PageHeading icon="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-like" title="Curated Recommendations" description="Discover Top Picks and Must-Have Gear Selected by Our Audiophile Experts" />
             <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet rote"></div> 0-300K IDR
+                {RECOMENDATION_CLASS.map((value) => (
+                    <Link key={value.key} href={`recomendation/${value.key}`} className="group text-sm card flex gap-2 items-center justify-center">
+                    <div className={`transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-${value.icon}`}></div> {value.class}
                 </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 300K-500K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 500K-1000K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 1000K-2000K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 2000K-3000K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 3000k-4000K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-wallet"></div> 4000K-5000K IDR
-                </Link>
-                <Link href="/" className="group text-sm card flex gap-2 items-center justify-center">
-                    <div className="transition-all group-hover:scale-110 group-hover:-rotate-12 bx bx-infinite"></div> UNLIMITED
-                </Link>
+                ))}
             </div>
         </div>
     )
---
+++ src/constant/recomendation.js
@@ -0,0 +1,42 @@
+export const RECOMENDATION_CLASS = [
+    {
+        key: 1,
+        class: '0-300K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 2,
+        class: '300K-500K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 3,
+        class: '500K-1000K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 4,
+        class: '1000K-2000K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 5,
+        class: '2000K-3000K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 6,
+        class: '3000K-4000K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 7,
+        class: '4000K-5000K IDR',
+        icon: 'wallet'
+    },
+    {
+        key: 8,
+        class: 'UNLIMITED',
+        icon: 'infinite'
+    },
+]
