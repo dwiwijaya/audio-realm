40ed711db2c9fb3093fe2bb6e9b62d330ede2842
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-20 21:05:36 +0700

    add: constant for term categories

--- src/components/view/home/HomeDictionary.jsx
+++ src/components/view/home/HomeDictionary.jsx
@@ -1,4 +1,6 @@
 import PageHeading from '@/components/common/PageHeading'
+import { TERM_CATEGORIES } from '@/constant/term'
+import Link from 'next/link'
 import React from 'react'
 
 const HomeDictionary = () => {
@@ -6,22 +8,12 @@ const HomeDictionary = () => {
     <div className=''>
       <PageHeading icon="bx bx-book" title="Explore Audiophile Glossary" description="Unlock the World of Audio Jargon with Our Comprehensive Dictionary" />
       <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-        <div className="group card">
-          <h3 className='font-bold text-sm whitespace-nowrap group-hover:tracking-wide transition-base group-hover:underline group-hover:underline-offset-4'>Sound Quality</h3>
-          <p className='text-xs mt-1'>Terms related to the overall audio performance</p>
-        </div>
-        <div className="group card">
-          <h3 className='font-bold text-sm whitespace-nowrap group-hover:tracking-wide transition-base group-hover:underline group-hover:underline-offset-4'>Technical Performance</h3>
-          <p className='text-xs mt-1'>Terms focusing to the technical specifications and performance</p>
-        </div>
-        <div className="group card">
-          <h3 className='font-bold text-sm whitespace-nowrap group-hover:tracking-wide transition-base group-hover:underline group-hover:underline-offset-4'>Design & Comfort</h3>
-          <p className='text-xs mt-1'>terms related to the physical design and comfort aspects</p>
-        </div>
-        <div className="group card">
-          <h3 className='font-bold text-sm whitespace-nowrap group-hover:tracking-wide transition-base group-hover:underline group-hover:underline-offset-4'>Others</h3>
-          <p className='text-xs mt-1'>{`Terms that don't fit into other categories but are still relevant`}</p>
-        </div>
+        {TERM_CATEGORIES.map((value) => (
+          <Link href={`/terms/${value.key}`} className="group card" key={value.key}>
+            <h3 className='font-bold text-sm whitespace-nowrap group-hover:tracking-wide transition-base group-hover:underline group-hover:underline-offset-4'>{value.title}</h3>
+            <p className='text-xs mt-1'>{value.description}</p>
+          </Link>
+        ))}
       </div>
     </div>
   )
---
+++ src/constant/term.js
@@ -0,0 +1,22 @@
+export const TERM_CATEGORIES = [
+    {
+        key: 'equipment',
+        title: 'Equipment',
+        description: 'Terms related to the various equipment and devices used in the audiophiles',
+    },
+    {
+        key: 'sound-quality',
+        title: 'Sound Quality',
+        description: 'Terms used to describe the characteristics and quality of sound',
+    },
+    {
+        key: 'audio-format',
+        title: 'Audio Formats',
+        description: 'Terms related to different types of music formats and media',
+    },
+    {
+        key: 'miscellaneous',
+        title: 'Miscellaneous',
+        description: `Terms that don't fit into other categories but are still relevant`,
+    },
+]
